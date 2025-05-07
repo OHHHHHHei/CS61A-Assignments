1d1ceea6bb5c048f1bc950ad31493a1507333dc4
OHHHHHHei <2859611604@qq.com>
2025-05-08 01:17:59 +0800

    first中的文档测试

--- Class_Code/first.py
+++ Class_Code/first.py
@@ -2,8 +2,12 @@
 from operator import floordiv, mod
 
 def divide_exact(n, d):
+    """Return the quotient and remainder of n divided by d.
+    
+    >>> q, r = divide_exact(2013, 10)
+    >>> q
+    201
+    >>> r
+    3
+    """
     return floordiv(n, d), mod(n, d)
-
-q, r = divide_exact(2013, 10)
-print('Quotient:', q)
-print('Remainder:', r)
